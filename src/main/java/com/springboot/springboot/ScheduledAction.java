35654683fbbf96ebfa9ff7c28de9f64f341bd0eb
hao <17343004135@163.com>
2019-11-18 15:12:18 +0800

    过滤器

--- src/main/java/com/springboot/springboot/ScheduledAction.java
+++ src/main/java/com/springboot/springboot/ScheduledAction.java
@@ -6,6 +6,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author: SJH
  * @ClassName: ScheduledAction
@@ -19,7 +22,8 @@ public class ScheduledAction {
     @Async//标记其为一个异步任务
     @Scheduled(cron = "*/2 * * * * ?")
     public void hello() {
-//        System.out.println("hello!: " + Thread.currentThread().getName());
-        System.out.println("ni  hao");
+       /* System.out.println("hello!: " + Thread.currentThread().getName());
+        String format = new SimpleDateFormat("yyyy--MM--ss").format(new Date());
+        System.out.println("******  ni  hao"+format+"****");*/
     }
 }
--- src/main/java/com/springboot/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/springboot/SpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 //@ServletComponentScan(basePackages = "com.springboot.springboot.servlet")//servlet注册
+@ServletComponentScan//在springBoot启动时会扫描@WebServlet，并将该类实例化
 public class SpringbootApplication {
 
     public static void main(String[] args) {
